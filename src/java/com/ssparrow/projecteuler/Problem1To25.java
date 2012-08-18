207973d33d0dd24ba9bf43f2d14477b953d82ac5
Fei Gao <eaglelion8038@hotmail.com>
2012-08-18 09:00:45 -0400

    add description for problem 20

--- src/java/com/ssparrow/projecteuler/Problem1To25.java
+++ src/java/com/ssparrow/projecteuler/Problem1To25.java
@@ -805,6 +805,17 @@ public class Problem1To25 {
 		return count;
 	}
 	
+	/**
+	 * n! means n  (n  1)  ...  3  2  1
+	 *
+	 *For example, 10! = 10  9  ...  3  2  1 = 3628800,
+	 *and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+	 *
+	 *Find the sum of the digits in the number 100!
+	 *
+	 * @param n
+	 * @return
+	 */
 	public static int p020FindSumOfDigitsInNto1(int n){
 		BigInteger result=BigInteger.ONE;
 		
